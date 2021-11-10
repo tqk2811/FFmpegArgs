17d4da1e2a8ae406e6c9499d1f0569c232c3f0b7
tqk2811 <tqk2811@gmail.com>
2021-11-10 18:28:09 +0700

    dev update filters

---
+++ FFmpegArgs.Filters/VideoFilters/DeconvolveFilter.cs
@@ -0,0 +1,51 @@
+﻿using FFmpegArgs.Cores.Filters;
+using FFmpegArgs.Cores.Maps;
+using FFmpegArgs.Filters.Enums;
+using FFmpegArgs.Utils;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace FFmpegArgs.Filters.VideoFilters
+{
+    /// <summary>
+    /// TS. deconvolve        VV->V      Deconvolve first video stream with second video stream.<br></br>
+    /// 
+    /// </summary>
+    public class DeconvolveFilter : ImageToImageFilter, ITimelineSupport, ISliceThreading, IFramesync
+    {
+        internal DeconvolveFilter(ImageMap imageMap, int planes) : base("deconvolve", imageMap)
+        {
+            AddMapOut();
+            this.SetOptionRange("planes", planes, 0, int.MaxValue);
+        }
+
+        /// <summary>
+        /// Set which impulse video frames will be processed, can be first or all. Default is all.
+        /// </summary>
+        /// <param name="impulse"></param>
+        /// <returns></returns>
+        public DeconvolveFilter Impulse(int impulse)
+            => this.SetOptionRange("impulse",impulse,0, int.MaxValue);
+
+        /// <summary>
+        /// Set noise when doing divisions. Default is 0.0000001. Useful when width and height are not same and not power of 2 or if stream prior to convolving had noise.
+        /// </summary>
+        /// <param name="noise"></param>
+        /// <returns></returns>
+        public DeconvolveFilter Noise(decimal noise)
+            => this.SetOptionRange("noise", noise, decimal.MinValue, decimal.MaxValue);
+
+    }
+
+    public static class DeconvolveFilterExtensions
+    {
+        /// <summary>
+        /// Apply 2D deconvolution of video stream in frequency domain using second stream as impulse.
+        /// </summary>
+        public static DeconvolveFilter DeconvolveFilter(this ImageMap imageMap, int planes)
+          => new DeconvolveFilter(imageMap, planes);
+    }
+}
---
+++ FFmpegArgs.Filters/VideoFilters/DedotFilter.cs
@@ -0,0 +1,74 @@
+﻿using FFmpegArgs.Cores.Filters;
+using FFmpegArgs.Cores.Maps;
+using FFmpegArgs.Filters.Enums;
+using FFmpegArgs.Utils;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace FFmpegArgs.Filters.VideoFilters
+{
+    /// <summary>
+    /// TS. dedot             V->V       Reduce cross-luminance and cross-color.<br></br>
+    /// https://ffmpeg.org/ffmpeg-filters.html#dedot
+    /// </summary>
+    public class DedotFilter : ImageToImageFilter, ITimelineSupport, ISliceThreading
+    {
+        internal DedotFilter(ImageMap imageMap) : base("dedot", imageMap)
+        {
+            AddMapOut();
+        }
+
+        /// <summary>
+        /// Set mode of operation. Can be combination of dotcrawl for cross-luminance reduction and/or rainbows for cross-color reduction.
+        /// </summary>
+        /// <param name="mode"></param>
+        /// <returns></returns>
+        public DedotFilter M(string mode)
+            => this.SetOption("m", mode);
+
+        /// <summary>
+        /// Set spatial luma threshold. Lower values increases reduction of cross-luminance.
+        /// </summary>
+        /// <param name="mode"></param>
+        /// <returns></returns>
+        public DedotFilter Lt(string mode)
+           => this.SetOption("lt", mode);
+
+        /// <summary>
+        /// Set tolerance for temporal luma.Higher values increases reduction of cross-luminance.
+        /// </summary>
+        /// <param name="mode"></param>
+        /// <returns></returns>
+        public DedotFilter Tl(string mode)
+           => this.SetOption("tl", mode);
+
+        /// <summary>
+        /// Set tolerance for chroma temporal variation. Higher values increases reduction of cross-color.
+        /// </summary>
+        /// <param name="mode"></param>
+        /// <returns></returns>
+        public DedotFilter Tc(string mode)
+           => this.SetOption("tc", mode);
+
+        /// <summary>
+        /// Set temporal chroma threshold. Lower values increases reduction of cross-color.
+        /// </summary>
+        /// <param name="mode"></param>
+        /// <returns></returns>
+        public DedotFilter Ct(string mode)
+           => this.SetOption("ct", mode);
+
+    }
+
+    public static class DedotFilterExtensions
+    {
+        /// <summary>
+        /// Reduce cross-luminance (dot-crawl) and cross-color (rainbows) from video.
+        /// </summary>
+        public static DedotFilter DedotFilter(this ImageMap imageMap)
+          => new DedotFilter(imageMap);
+    }
+}
