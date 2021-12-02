de5d82f2e18e08ab7d5682dcb15874338e80b0f4
tqk2811 <tqk2811@gmail.com>
2021-12-02 17:25:23 +0700

    Filter: DrawTextFilterConfig

--- FFmpegArgs.Filters/VideoFilters/DrawTextFilter.cs
+++ FFmpegArgs.Filters/VideoFilters/DrawTextFilter.cs
@@ -33,11 +33,30 @@ namespace FFmpegArgs.Filters.VideoFilters
         };
 
         readonly Expression expression = new Expression(_variables);
-        internal DrawTextFilter(string text, ImageMap imageMap) : base("drawtext", imageMap)
+        internal DrawTextFilter(ImageMap imageMap) : base("drawtext", imageMap)
         {
             AddMapOut();
-            this.SetOption("text", text);
         }
+
+        /// <summary>
+        /// The text string to be drawn. The text must be a sequence of UTF-8 encoded characters. This parameter is mandatory if no file is specified with the parameter textfile.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public DrawTextFilter Text(string text)
+            => this.SetOption("text", text);
+
+
+        /// <summary>
+        /// A text file containing text to be drawn. The text must be a sequence of UTF-8 encoded characters.<br>
+        /// </br>This parameter is mandatory if no text string is specified with the parameter text.<br>
+        /// </br>If both text and textfile are specified, an error is thrown.
+        /// </summary>
+        /// <param name="textFile"></param>
+        /// <returns></returns>
+        public DrawTextFilter TextFile(string textFile)
+            => this.SetOption("textfile", textFile);
+
 
         /// <summary>
         /// Used to draw a box around text using the background color. The value must be either 1 (enable) or 0 (disable). The default value of box is 0.
@@ -157,16 +176,16 @@ namespace FFmpegArgs.Filters.VideoFilters
         /// </summary>
         /// <param name="alpha"></param>
         /// <returns></returns>
-        public DrawTextFilter Alpha(float alpha)
-            => this.SetOptionRange("alpha", alpha, 0, 1);
+        public DrawTextFilter Alpha(string alpha)
+            => this.SetOption("alpha", alpha);
 
         /// <summary>
         /// The font size to be used for drawing text. The default value of fontsize is 16.
         /// </summary>
         /// <param name="size"></param>
         /// <returns></returns>
-        public DrawTextFilter FontSize(int size)
-            => this.SetOptionRange("fontsize", size, 0, int.MaxValue);
+        public DrawTextFilter FontSize(string size)
+            => this.SetOption("fontsize", size);
 
         /// <summary>
         /// If set to 1, attempt to shape the text (for example, reverse the order of right-to-left text and join Arabic characters) before drawing it. Otherwise, just draw the text exactly as given. By default 1 (if supported).
@@ -196,11 +215,23 @@ namespace FFmpegArgs.Filters.VideoFilters
         /// <summary>
         /// The x and y offsets for the text shadow position with respect to the position of the text. They can be either positive or negative values. The default value for both is "0". (from INT_MIN to INT_MAX) (default 0)
         /// </summary>
+        public DrawTextFilter ShadowX(int x)
+           => this.SetOptionRange("x", x, INT_MIN, INT_MAX);
+
+        /// <summary>
+        /// The x and y offsets for the text shadow position with respect to the position of the text. They can be either positive or negative values. The default value for both is "0". (from INT_MIN to INT_MAX) (default 0)
+        /// </summary>
+        public DrawTextFilter ShadowY(int y)
+          => this.SetOptionRange("y", y, INT_MIN, INT_MAX);
+
+        /// <summary>
+        /// The x and y offsets for the text shadow position with respect to the position of the text. They can be either positive or negative values. The default value for both is "0". (from INT_MIN to INT_MAX) (default 0)
+        /// </summary>
         /// <param name="x"></param>
         /// <param name="y"></param>
         /// <returns></returns>
         public DrawTextFilter ShadowXY(int x, int y)
-            => this.SetOptionRange("x", x, INT_MIN, INT_MAX).SetOptionRange("y", y, INT_MIN, INT_MAX);
+            => this.ShadowX(x).ShadowY(y);
 
         /// <summary>
         /// The starting frame number for the n/frame_num variable. (from 0 to INT_MAX) (default 0)
@@ -222,8 +253,8 @@ namespace FFmpegArgs.Filters.VideoFilters
         /// </summary>
         /// <param name="time"></param>
         /// <returns></returns>
-        public DrawTextFilter TimeCode(TimeSpan time)
-            => this.SetOption("timecode", time.ToString("hh\\:mm\\:ss\\.fff"));
+        public DrawTextFilter TimeCode(string timeCode)
+            => this.SetOption("timecode", timeCode);
 
         /// <summary>
         /// Set the timecode frame rate (timecode only). Value will be rounded to nearest integer. Minimum value is "1". Drop-frame timecode is supported for frame rates 30 & 60.
@@ -245,11 +276,30 @@ namespace FFmpegArgs.Filters.VideoFilters
         /// The expressions which specify the offsets where text will be drawn within the video frame. They are relative to the top/left border of the output image.<br>
         /// </br>The default value of x and y is "0".
         /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <returns></returns>
-        public DrawTextFilter Pos(Action<Expression> x, Action<Expression> y)
-          => this.SetOption("x", x.Run(expression)).SetOption("y", y.Run(expression));
+        public DrawTextFilter X(string x)
+            => this.X(x.Expression());
+
+        /// <summary>
+        /// The expressions which specify the offsets where text will be drawn within the video frame. They are relative to the top/left border of the output image.<br>
+        /// </br>The default value of x and y is "0".
+        /// </summary>
+        public DrawTextFilter X(Action<Expression> x)
+            => this.SetOption("x", x.Run(expression));
+
+        /// <summary>
+        /// The expressions which specify the offsets where text will be drawn within the video frame. They are relative to the top/left border of the output image.<br>
+        /// </br>The default value of x and y is "0".
+        /// </summary>
+        public DrawTextFilter Y(string y)
+            => this.Y(y.Expression());
+
+        /// <summary>
+        /// The expressions which specify the offsets where text will be drawn within the video frame. They are relative to the top/left border of the output image.<br>
+        /// </br>The default value of x and y is "0".
+        /// </summary>
+        public DrawTextFilter Y(Action<Expression> y)
+            => this.SetOption("y", y.Run(expression));
+
         /// <summary>
         /// The expressions which specify the offsets where text will be drawn within the video frame. They are relative to the top/left border of the output image.<br>
         /// </br>The default value of x and y is "0".
@@ -257,8 +307,26 @@ namespace FFmpegArgs.Filters.VideoFilters
         /// <param name="x"></param>
         /// <param name="y"></param>
         /// <returns></returns>
-        public DrawTextFilter Pos(string x, string y)
-          => Pos(x.Expression(), y.Expression());
+        public DrawTextFilter XY(Action<Expression> x, Action<Expression> y)
+          => this.X(x).Y(y);
+
+        /// <summary>
+        /// The expressions which specify the offsets where text will be drawn within the video frame. They are relative to the top/left border of the output image.<br>
+        /// </br>The default value of x and y is "0".
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public DrawTextFilter XY(string x, string y)
+          => this.X(x).Y(y);
+
+        /// <summary>
+        /// If set to 1, the textfile will be reloaded before each frame. Be sure to update it atomically, or it may be read partially, or even fail.
+        /// </summary>
+        /// <param name="reload"></param>
+        /// <returns></returns>
+        public DrawTextFilter Reload(bool reload)
+         => this.SetOption("reload", reload.ToFFmpegFlag());
     }
 
     public static class DrawTextFilterExtension
@@ -269,10 +337,172 @@ namespace FFmpegArgs.Filters.VideoFilters
         /// <param name="imageMap"></param>
         /// <param name="text"></param>
         /// <returns></returns>
-        public static DrawTextFilter DrawTextFilter(this ImageMap imageMap, string text)
-            => new DrawTextFilter(text, imageMap);
+        public static DrawTextFilter DrawTextFilter(this ImageMap imageMap)
+            => new DrawTextFilter(imageMap);
+
+        public static DrawTextFilter DrawtextFilterGen(this ImageMap imageMap, DrawTextFilterConfig config)
+        {
+            var result = new DrawTextFilter(imageMap);
+            if (!string.IsNullOrWhiteSpace(config?.FontFile)) result.FontFile(config.FontFile);
+            if (!string.IsNullOrWhiteSpace(config?.Text)) result.Text(config.Text);
+            if (!string.IsNullOrWhiteSpace(config?.TextFile)) result.TextFile(config.TextFile);
+            if (config?.FontColor != null) result.FontColor(config.FontColor.Value);
+            if (!string.IsNullOrWhiteSpace(config?.FontColor_Expr)) result.FontColorExpr(config.FontColor_Expr);
+            if (config?.Boxcolor != null) result.Boxcolor(config.Boxcolor.Value);
+            if (config?.Bordercolor != null) result.Bordercolor(config.Bordercolor.Value);
+            if (config?.ShadowColor != null) result.Shadowcolor(config.ShadowColor.Value);
+            if (config?.Box != null) result.Box(config.Box.Value);
+            if (config?.BoxBorderW != null) result.Boxborderw(config.BoxBorderW.Value);
+            if (config?.LineSpacing != null) result.LineSpacing(config.LineSpacing.Value);
+            if (!string.IsNullOrWhiteSpace(config?.FontSize)) result.FontSize(config.FontSize);
+            if (!string.IsNullOrWhiteSpace(config?.X)) result.X(config.X);
+            if (!string.IsNullOrWhiteSpace(config?.Y)) result.Y(config.Y);
+            if (config?.ShadowX != null) result.ShadowX(config.ShadowX.Value);
+            if (config?.ShadowY != null) result.ShadowY(config.ShadowY.Value);
+            if (config?.BorderW != null) result.Borderw(config.BorderW.Value);
+            if (config?.TabSize != null) result.TabSize(config.TabSize.Value);
+            if (config?.BaseTime != null) result.Basetime(config.BaseTime.Value);
+            if (!string.IsNullOrWhiteSpace(config?.Font)) result.Font(config.Font);
+            if (config?.Expansion != null) result.Expansion(config.Expansion.Value);
+            if (!string.IsNullOrWhiteSpace(config?.TimeCode)) result.TimeCode(config.TimeCode);
+            if (config?.Tc24hMax != null) result.Tc24hMax(config.Tc24hMax.Value);
+            if (config?.TimeCodeRate != null) result.TimeCodeRate(config.TimeCodeRate);
+            if (config?.Reload != null) result.Reload(config.Reload.Value);
+            if (!string.IsNullOrWhiteSpace(config?.Alpha)) result.Alpha(config.Alpha);
+            if (config?.FixBounds != null) result.FixBounds(config.FixBounds.Value);
+            if (config?.StartNumber != null) result.StartNumber(config.StartNumber.Value);
+            if (config?.TextShaping != null) result.TextShaping(config.TextShaping.Value);
+            if (config?.FtLoadFlags != null) result.FtLoadFlags(config.FtLoadFlags.Value);
+            if (!string.IsNullOrWhiteSpace(config?.TimelineSupport)) result.Enable(config.TimelineSupport);
+            return result;
+        }
     }
 
+
+    public class DrawTextFilterConfig : ITimelineSupportConfig
+    {
+        /// <summary>
+        ///  set font file
+        /// </summary>
+        public string FontFile { get; set; }
+        /// <summary>
+        ///  set text
+        /// </summary>
+        public string Text { get; set; }
+        /// <summary>
+        ///  set text file
+        /// </summary>
+        public string TextFile { get; set; }
+        /// <summary>
+        ///  set foreground color (default "black")
+        /// </summary>
+        public Color? FontColor { get; set; }
+        /// <summary>
+        ///  set foreground color expression (default "")
+        /// </summary>
+        public string FontColor_Expr { get; set; }
+        /// <summary>
+        ///  set box color (default "white")
+        /// </summary>
+        public Color? Boxcolor { get; set; }
+        /// <summary>
+        ///  set border color (default "black")
+        /// </summary>
+        public Color? Bordercolor { get; set; }
+        /// <summary>
+        ///  set shadow color (default "black")
+        /// </summary>
+        public Color? ShadowColor { get; set; }
+        /// <summary>
+        ///  set box (default false)
+        /// </summary>
+        public bool? Box { get; set; }
+        /// <summary>
+        ///  set box border width (from INT_MIN to INT_MAX) (default 0)
+        /// </summary>
+        public int? BoxBorderW { get; set; }
+        /// <summary>
+        ///  set line spacing in pixels (from INT_MIN to INT_MAX) (default 0)
+        /// </summary>
+        public int? LineSpacing { get; set; }
+        /// <summary>
+        ///  set font size
+        /// </summary>
+        public string FontSize { get; set; }
+        /// <summary>
+        ///  set x expression (default "0")
+        /// </summary>
+        public string X { get; set; }
+        /// <summary>
+        ///  set y expression (default "0")
+        /// </summary>
+        public string Y { get; set; }
+        /// <summary>
+        ///  set shadow x offset (from INT_MIN to INT_MAX) (default 0)
+        /// </summary>
+        public int? ShadowX { get; set; }
+        /// <summary>
+        ///  set shadow y offset (from INT_MIN to INT_MAX) (default 0)
+        /// </summary>
+        public int? ShadowY { get; set; }
+        /// <summary>
+        ///  set border width (from INT_MIN to INT_MAX) (default 0)
+        /// </summary>
+        public int? BorderW { get; set; }
+        /// <summary>
+        ///  set tab size (from 0 to INT_MAX) (default 4)
+        /// </summary>
+        public int? TabSize { get; set; }
+        /// <summary>
+        ///  set base time (from I64_MIN to I64_MAX) (default I64_MIN)
+        /// </summary>
+        public long? BaseTime { get; set; }
+        /// <summary>
+        ///  Font name (default "Sans")
+        /// </summary>
+        public string Font { get; set; }
+        /// <summary>
+        ///  set the expansion mode (from 0 to 2) (default normal)
+        /// </summary>
+        public DrawTextExpansion? Expansion { get; set; }
+        /// <summary>
+        ///  set initial timecode
+        /// </summary>
+        public string TimeCode { get; set; }
+        /// <summary>
+        ///  set 24 hours max (timecode only) (default false)
+        /// </summary>
+        public bool? Tc24hMax { get; set; }
+        /// <summary>
+        ///  set rate (timecode only) (from 0 to INT_MAX) (default 0/1)
+        /// </summary>
+        public Rational TimeCodeRate { get; set; }
+        /// <summary>
+        ///  reload text file for each frame (default false)
+        /// </summary>
+        public bool? Reload { get; set; }
+        /// <summary>
+        ///  apply alpha while rendering (default "1")
+        /// </summary>
+        public string Alpha { get; set; }
+        /// <summary>
+        ///  check and fix text coords to avoid clipping (default false)
+        /// </summary>
+        public bool? FixBounds { get; set; }
+        /// <summary>
+        ///  start frame number for n/frame_num variable (from 0 to INT_MAX) (default 0)
+        /// </summary>
+        public int? StartNumber { get; set; }
+        /// <summary>
+        ///  attempt to shape text before drawing (default true)
+        /// </summary>
+        public bool? TextShaping { get; set; }
+        /// <summary>
+        ///  set font loading flags for libfreetype (default 0)
+        /// </summary>
+        public DrawTextFtLoadFlags? FtLoadFlags { get; set; }
+        public string TimelineSupport { get; set; }
+    }
     public enum DrawTextExpansion
     {
         none,
