d5da61ebdb6e9118a0cbfe73a9607ac58f2e2f4b
tqk2811 <tqk2811@gmail.com>
2021-12-03 17:26:08 +0700

    Fix RenderProgress

--- FFmpegArgs.Executes/RenderProgress.cs
+++ FFmpegArgs.Executes/RenderProgress.cs
@@ -9,19 +9,22 @@ namespace FFmpegArgs.Executes
             = new Regex(@"frame= *(\d+) fps= *([0-9.]+).*?size= *([0-9.]+)kB time= *(\d{2}:\d{2}:\d{2}.\d+) bitrate= *([0-9.]+)kbits\/s speed= *([0-9.]+)x");
         public static RenderProgress FromProgressString(string progress)
         {
-            Match match = regex.Match(progress);
-            if (match.Success && match.Groups.Count == 7)
+            if(!string.IsNullOrWhiteSpace(progress))
             {
-                return new RenderProgress
+                Match match = regex.Match(progress);
+                if (match.Success && match.Groups.Count == 7)
                 {
-                    Frame = int.Parse(match.Groups[1].Value),
-                    Fps = double.Parse(match.Groups[2].Value),
-                    Size = double.Parse(match.Groups[3].Value),
-                    Time = TimeSpan.Parse(match.Groups[4].Value),
-                    Bitrate = double.Parse(match.Groups[5].Value),
-                    Speed = float.Parse(match.Groups[6].Value)
-                };
-            }
+                    return new RenderProgress
+                    {
+                        Frame = int.Parse(match.Groups[1].Value),
+                        Fps = double.Parse(match.Groups[2].Value),
+                        Size = double.Parse(match.Groups[3].Value),
+                        Time = TimeSpan.Parse(match.Groups[4].Value),
+                        Bitrate = double.Parse(match.Groups[5].Value),
+                        Speed = float.Parse(match.Groups[6].Value)
+                    };
+                }
+            }            
             return null;
         }
 
