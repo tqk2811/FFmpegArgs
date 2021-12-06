bf345f924c6b19af6682fb6184d14f8dac3d5021
tqk2811 <tqk2811@gmail.com>
2021-12-06 10:44:55 +0700

    update IFramesync summary

--- FFmpegArgs/Filters/IFramesync.cs
+++ FFmpegArgs/Filters/IFramesync.cs
@@ -11,6 +11,7 @@ namespace FFmpegArgs.Filters
     {
         /// <summary>
         /// https://ffmpeg.org/ffmpeg-filters.html#Options-for-filters-with-several-inputs-_0028framesync_0029 <br></br>
+        /// The action to take when EOF is encountered on the secondary input
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="filter"></param>
@@ -20,10 +21,24 @@ namespace FFmpegArgs.Filters
             where T : BaseOption, IFilter<IMap, IMap>, IFramesync
           => filter.SetOption("eof_action", action.ToString().ToLower());
 
+        /// <summary>
+        /// If set to 1, force the output to terminate when the shortest input terminates. Default value is 0.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="filter"></param>
+        /// <param name="flag"></param>
+        /// <returns></returns>
         public static T Shortest<T>(this T filter, bool flag)
              where T : BaseOption, IFilter<IMap, IMap>, IFramesync
           => filter.SetOption("shortest", flag.ToFFmpegFlag());
 
+        /// <summary>
+        /// If set to 1, force the filter to extend the last frame of secondary streams until the end of the primary stream. A value of 0 disables this behavior. Default value is 1.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="filter"></param>
+        /// <param name="flag"></param>
+        /// <returns></returns>
         public static T Repeatlast<T>(this T filter, bool flag)
              where T : BaseOption, IFilter<IMap, IMap>, IFramesync
           => filter.SetOption("repeatlast", flag.ToFFmpegFlag());
