1b8e9a70e319aaafc040ba8048cea967c84322af
tqk2811 <tqk2811@gmail.com>
2021-12-06 20:42:20 +0700

    Filter: Add AtrimFilter

---
+++ FFmpegArgs.Filters/AudioFilters/AtrimFilter.cs
@@ -0,0 +1,115 @@
+﻿using FFmpegArgs.Cores.Filters;
+using FFmpegArgs.Cores.Maps;
+using FFmpegArgs.Filters.Enums;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace FFmpegArgs.Filters.AudioFilters
+{
+    /// <summary>
+    /// ... atrim             A->A       Pick one continuous section from the input, drop the rest.<br></br>
+    /// https://ffmpeg.org/ffmpeg-filters.html#atrim
+    /// </summary>
+    public class AtrimFilter : AudioToAudioFilter
+    {
+        internal AtrimFilter(AudioMap audioMap) : base("atrim", audioMap)
+        {
+            AddMapOut();
+        }
+
+        /// <summary>
+        /// Timestamp (in seconds) of the start of the section to keep. I.e. the audio sample with the timestamp start will be the first sample in the output.
+        /// </summary>
+        /// <param name="start">..F.A...... Timestamp of the first frame that should be passed (default INT64_MAX)</param>
+        /// <returns></returns>
+        public AtrimFilter Start(TimeSpan start)
+            => this.SetOptionRange("start", start, TimeSpan.Zero, TimeSpan.MaxValue);
+
+        /// <summary>
+        /// Specify time of the first audio sample that will be dropped, i.e. the audio sample immediately preceding the one with the timestamp end will be the last sample in the output.
+        /// </summary>
+        /// <param name="end">..F.A...... Timestamp of the first frame that should be dropped again (default INT64_MAX)</param>
+        /// <returns></returns>
+        public AtrimFilter End(TimeSpan end)
+            => this.SetOptionRange("end", end, TimeSpan.Zero, TimeSpan.MaxValue);
+
+        /// <summary>
+        /// Same as start, except this option sets the start timestamp in samples instead of seconds.
+        /// </summary>
+        /// <param name="start_pts">..F.A...... Timestamp of the first frame that should be  passed (from I64_MIN to I64_MAX) (default I64_MIN)</param>
+        /// <returns></returns>
+        public AtrimFilter StartPts(long start_pts)
+            => this.SetOptionRange("start_pts", start_pts, I64_MIN, I64_MAX);
+
+        /// <summary>
+        /// Same as end, except this option sets the end timestamp in samples instead of seconds.
+        /// </summary>
+        /// <param name="end_pts">..F.A...... Timestamp of the first frame that should be dropped again (from I64_MIN to I64_MAX) (default I64_MIN)</param>
+        /// <returns></returns>
+        public AtrimFilter EndPts(long end_pts)
+           => this.SetOptionRange("end_pts", end_pts, I64_MIN, I64_MAX);
+
+        /// <summary>
+        /// The maximum duration of the output in seconds.
+        /// </summary>
+        /// <param name="start">..F.A...... Maximum duration of the output (default 0)</param>
+        /// <returns></returns>
+        public AtrimFilter Duration(TimeSpan duration)
+           => this.SetOptionRange("duration", duration, TimeSpan.Zero, TimeSpan.MaxValue);
+
+        /// <summary>
+        /// The number of the first sample that should be output.
+        /// </summary>
+        /// <param name="start_sample">..F.A...... Number of the first audio sample that should be passed to the output (from -1 to I64_MAX) (default -1)</param>
+        /// <returns></returns>
+        public AtrimFilter StartSample(long start_sample)
+           => this.SetOptionRange("start_sample", start_sample, -1, I64_MAX);
+
+        /// <summary>
+        /// The number of the first sample that should be dropped
+        /// </summary>
+        /// <param name="end_sample">..F.A...... Number of the first audio sample that should be dropped again (from 0 to I64_MAX) (default I64_MAX)</param>
+        /// <returns></returns>
+        public AtrimFilter EndSample(long end_sample)
+           => this.SetOptionRange("end_sample", end_sample, 0, I64_MAX);
+    }
+
+    public static class AtrimFilterExtensions
+    {
+        /// <summary>
+        /// Trim the input so that the output contains one continuous subpart of the input.
+        /// </summary>
+        public static AtrimFilter AtrimFilter(this AudioMap audioMap)
+          => new AtrimFilter(audioMap);
+
+        /// <summary>
+        /// Trim the input so that the output contains one continuous subpart of the input.
+        /// </summary>
+        public static AtrimFilter AtrimFilter(this AudioMap audioMap, AtrimFilterConfig config)
+        {
+            var result = new AtrimFilter(audioMap);
+            if (config?.Start != null) result.Start(config.Start.Value);
+            if (config?.End != null) result.End(config.End.Value);
+            if (config?.StartPts != null) result.StartPts(config.StartPts.Value);
+            if (config?.EndPts != null) result.EndPts(config.EndPts.Value);
+            if (config?.Duration != null) result.Duration(config.Duration.Value);
+            if (config?.StartSample != null) result.StartSample(config.StartSample.Value);
+            if (config?.EndSample != null) result.EndSample(config.EndSample.Value);
+            return result;
+        }
+    }
+
+    public class AtrimFilterConfig
+    {
+        public TimeSpan? Start { get; set; }
+        public TimeSpan? End { get; set; }
+        public long? StartPts { get; set; }
+        public long? EndPts { get; set; }
+        public TimeSpan? Duration { get; set; }
+        public long? StartSample { get; set; }
+        public long? EndSample { get; set; }
+    }
+}
