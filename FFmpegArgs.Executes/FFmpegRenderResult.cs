ee809cf65aaf86e797d7ad319d3f0fa5ce92d373
tqk2811 <tqk2811@gmail.com>
2021-12-09 10:43:38 +0700

    Executes: FFmpegRenderResult Add ErrorDatas

--- FFmpegArgs.Executes/FFmpegRenderResult.cs
+++ FFmpegArgs.Executes/FFmpegRenderResult.cs
@@ -6,9 +6,10 @@ namespace FFmpegArgs.Executes
     {
         public string Arguments { get; internal set; }
         internal List<string> _OutputDatas { get; } = new List<string>();
+        internal List<string> _ErrorDatas { get; } = new List<string>();
         public int ExitCode { get; internal set; } = 0;
         public IEnumerable<string> OutputDatas { get { return _OutputDatas; } }
-
+        public IEnumerable<string> ErrorDatas { get { return _OutputDatas; } }
         public FFmpegRenderResult EnsureSuccess()
         {
             if (ExitCode != 0) throw new FFmpegRenderException(ExitCode, OutputDatas);
