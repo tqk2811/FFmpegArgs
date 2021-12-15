eb3dc93928ed8cd7ca6293e7448c5224acf2f044
tqk2811 <tqk2811@gmail.com>
2021-12-15 10:20:34 +0700

    fix RenderProgress Regex

--- FFmpegArgs.Executes/RenderProgress.cs
+++ FFmpegArgs.Executes/RenderProgress.cs
@@ -6,7 +6,7 @@ namespace FFmpegArgs.Executes
     public class RenderProgress
     {
         static readonly Regex regex
-            = new Regex(@"frame= *(\d+) fps= *([0-9.]+).*?size= *([0-9.]+)kB.*?time= *(\d{2}:\d{2}:\d{2}\.\d+).*?bitrate= *([0-9.]+)kbits\/s.*?speed= *([0-9.]+)x");
+            = new Regex(@"frame= *(\d+) fps= *([0-9.]+).*?size= *(N\/A|[0-9.]+kB).*?time= *(\d{2}:\d{2}:\d{2}\.\d+).*?bitrate= *(N\/A|[0-9.]+kbits\/s).*?speed= *([0-9.]+)x");
         public static RenderProgress FromProgressString(string progress)
         {
             if(!string.IsNullOrWhiteSpace(progress))
@@ -18,13 +18,13 @@ namespace FFmpegArgs.Executes
                     {
                         Frame = int.Parse(match.Groups[1].Value),
                         Fps = double.Parse(match.Groups[2].Value),
-                        Size = double.Parse(match.Groups[3].Value),
+                        Size = match.Groups[3].Value.Equals("N/A") ? double.NaN : double.Parse(match.Groups[3].Value.Replace("kB",string.Empty)),
                         Time = TimeSpan.Parse(match.Groups[4].Value),
-                        Bitrate = double.Parse(match.Groups[5].Value),
+                        Bitrate = match.Groups[5].Value.Equals("N/A") ? double.NaN : double.Parse(match.Groups[5].Value.Replace("kbits/s", string.Empty)),
                         Speed = float.Parse(match.Groups[6].Value)
                     };
                 }
-            }            
+            }
             return null;
         }
 
