4771387335d2919a863b7e147870b46e3fdd5252
tqk2811 <tqk2811@gmail.com>
2021-12-15 16:35:00 +0700

    fix FFmpegRenderResult.ErrorDatas

--- FFmpegArgs.Executes/FFmpegRenderResult.cs
+++ FFmpegArgs.Executes/FFmpegRenderResult.cs
@@ -9,7 +9,7 @@ namespace FFmpegArgs.Executes
         internal List<string> _ErrorDatas { get; } = new List<string>();
         public int ExitCode { get; internal set; } = 0;
         public IEnumerable<string> OutputDatas { get { return _OutputDatas; } }
-        public IEnumerable<string> ErrorDatas { get { return _OutputDatas; } }
+        public IEnumerable<string> ErrorDatas { get { return _ErrorDatas; } }
         public FFmpegRenderResult EnsureSuccess()
         {
             if (ExitCode != 0) throw new FFmpegRenderException(ExitCode, OutputDatas);
