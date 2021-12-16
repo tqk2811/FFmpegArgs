cbde015a058772014e732a6c2261fec32807c5f8
tqk2811 <tqk2811@gmail.com>
2021-12-16 11:23:03 +0700

    Filter: add asetrate

---
+++ FFmpegArgs.Filters/AudioFilters/AsetrateFilter.cs
@@ -0,0 +1,56 @@
+﻿using FFmpegArgs.Cores.Filters;
+using FFmpegArgs.Cores.Maps;
+using FFmpegArgs.Filters.Enums;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace FFmpegArgs.Filters.AudioFilters
+{
+    /// <summary>
+    /// ... asetrate          A->A       Change the sample rate without altering the data.<br></br>
+    /// https://ffmpeg.org/ffmpeg-filters.html#asetrate
+    /// </summary>
+    public class AsetrateFilter : AudioToAudioFilter
+    {
+        internal AsetrateFilter(AudioMap audioMap) : base("asetrate", audioMap)
+        {
+            AddMapOut();
+        }
+
+        /// <summary>
+        /// Set the output sample rate. Default is 44100 Hz.
+        /// </summary>
+        /// <param name="r"></param>
+        /// <returns></returns>
+        public AsetrateFilter SampleRate(int r)
+            => this.SetOptionRange("r", r, 0, INT_MAX);
+
+    }
+
+    public static class AsetrateFilterExtensions
+    {
+        /// <summary>
+        /// Set the sample rate without altering the PCM data. This will result in a change of speed and pitch.
+        /// </summary>
+        public static AsetrateFilter AsetrateFilter(this AudioMap audioMap)
+          => new AsetrateFilter(audioMap);
+
+        /// <summary>
+        /// Set the sample rate without altering the PCM data. This will result in a change of speed and pitch.
+        /// </summary>
+        public static AsetrateFilter AsetrateFilter(this AudioMap audioMap, AsetrateFilterConfig config)
+        {
+            var result = new AsetrateFilter(audioMap);
+            if (config?.SampleRate != null) result.SampleRate(config.SampleRate.Value);
+            return result;
+        }
+    }
+
+    public class AsetrateFilterConfig
+    {
+        public int? SampleRate { get; set; }
+    }
+}
