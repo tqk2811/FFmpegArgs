d52236376f21847cd9d5a298d48f0e5b429048b3
tqk2811 <tqk2811@gmail.com>
2021-12-31 01:04:48 +0700

    Test: fix photocolection

--- FFmpegArgs.Test/TanersenerSlideShow/PhotoCollection.cs
+++ FFmpegArgs.Test/TanersenerSlideShow/PhotoCollection.cs
@@ -32,6 +32,13 @@ namespace FFmpegArgs.Test.TanersenerSlideShow
 
             Config config = new Config();
             int MAX_IMAGE_ANGLE = 25;
+            double RPS = 1;//Revolutions per sec
+            int WIDTH = config.Size.Width;
+            int HEIGHT = config.Size.Height;
+            int FPS = config.Fps;
+            Color BACKGROUND_COLOR = config.BackgroundColor;
+            double TRANSITION_DURATION = config.TransitionDuration.TotalSeconds;
+
             TimeSpan TOTAL_DURATION = (config.ImageDuration + config.TransitionDuration) * images_inputmap.Count;
 
             Random random = new Random();
@@ -44,31 +51,29 @@ namespace FFmpegArgs.Test.TanersenerSlideShow
                 .FpsFilter($"{config.Fps}").MapOut;
 
             var lastOverLay = background;
-
-            for(int c = 0; c < images_inputmap.Count; c++)
+            images_inputmap = images_inputmap.InputScreenMode(ScreenMode.Center, config);
+            for (int c = 0; c < images_inputmap.Count; c++)
             {
                 var start = (config.TransitionDuration + config.ImageDuration) * c;
                 var end = start + config.TransitionDuration;
                 var ANGLE_RANDOMNESS = random.Next() % MAX_IMAGE_ANGLE + 1;
 
                 lastOverLay = images_inputmap[c]
-                    .SetPtsFilter("PTS-STARTPTS").MapOut
-                    .ScaleFilter($"if(gte(iw/ih,{config.Size.Width}/{config.Size.Height}),min(iw,{config.Size.Width}),-1)", $"if(gte(iw/ih,{config.Size.Width}/{config.Size.Height}),-1,min(ih,{config.Size.Height}))").MapOut
-                    .ScaleFilter("trunc(iw/2)*2", "trunc(ih/2)*2").MapOut
-                    .SetSarFilter("1/1").MapOut
-                    .FpsFilter($"{config.Fps}").MapOut
-                    .FormatFilter(PixFmt.rgba).MapOut
-                    .PadFilter($"{config.Size.Width * 4}", $"{config.Size.Height}").Position($"({config.Size.Width * 4}-iw)/2", $"({config.Size.Height}-ih)/2").Color(config.BackgroundColor).MapOut
+                    .PadFilter($"{WIDTH * 4}", $"{HEIGHT}").Position($"({WIDTH * 4}-iw)/2", $"({HEIGHT}-ih)/2").Color(BACKGROUND_COLOR).MapOut
                     .TrimFilter().Duration((c + 1) * (config.TransitionDuration + config.ImageDuration)).MapOut
                     .SetPtsFilter("PTS-STARTPTS").MapOut
-                    .RotateFilter($"if(between(t,{start.TotalSeconds},{end.TotalSeconds})," +
-                                        $"2*PI*t+if(eq(mod({c},2),0),1,-1)*{ANGLE_RANDOMNESS}*PI/180," +
-                                        $"if(eq(mod({c},2),0),1,-1)*{ANGLE_RANDOMNESS}*PI/180)")
-                        .OW($"{config.Size.Width * 4}").FillColor(config.BackgroundColor).MapOut
+
+
+                    .RotateFilter(  $"if(" +
+                                        $"between(t,{start.TotalSeconds},{end.TotalSeconds})," +
+                                        $"{RPS}*2*PI*(t-{end.TotalSeconds})," +
+                                        $"0)" +
+                                    $"+if(eq(mod({c},2),0),1,-1)*{ANGLE_RANDOMNESS}*2*PI/360")
+                        .OW($"{WIDTH * 4}").FillColor(BACKGROUND_COLOR).MapOut
                     .OverlayFilterOn(lastOverLay,
                         $"if(gt(t,{start.TotalSeconds})," +
                             $"if(lt(t,{end.TotalSeconds})," +
-                                $"{config.Size.Width}*3/2 -w+(t-{start.TotalSeconds})/{config.TransitionDuration.TotalSeconds}*{config.Size.Width}," +
+                                $"{WIDTH}*3/2 -w+(t-{start.TotalSeconds})/{TRANSITION_DURATION}*{WIDTH}," +
                                 "(main_w-overlay_w)/2)," +
                             "-w)",
                         "(main_h-overlay_h)/2").MapOut;
@@ -101,6 +106,7 @@ namespace FFmpegArgs.Test.TanersenerSlideShow
 
             Config config = new Config();
             int MAX_IMAGE_ANGLE = 25;
+            double RPS = 1.2;
             TimeSpan TOTAL_DURATION = (config.ImageDuration + config.TransitionDuration) * images_inputmap.Count;
             
             Random random = new Random();
@@ -111,27 +117,24 @@ namespace FFmpegArgs.Test.TanersenerSlideShow
                 .ImageMaps.First().TrimFilter().Duration(TOTAL_DURATION).MapOut;
 
             var lastOverLay = background;
+            images_inputmap = images_inputmap.InputScreenMode(ScreenMode.Center, config);
 
-            for(int c = 0; c < images_inputmap.Count; c++)
+            for (int c = 0; c < images_inputmap.Count; c++)
             {
                 var ANGLE_RANDOMNESS = random.Next() % MAX_IMAGE_ANGLE + 1;
                 var start = (config.TransitionDuration + config.ImageDuration) * c;
                 var end = start + config.TransitionDuration;
                 lastOverLay = images_inputmap[c]
-                    .SetPtsFilter("PTS-STARTPTS").MapOut
-                    .ScaleFilter($"if(gte(iw/ih,{config.Size.Width}/{config.Size.Height}),min(iw,{config.Size.Width}),-1)", $"if(gte(iw/ih,{config.Size.Width}/{config.Size.Height}),-1,min(ih,{config.Size.Height}))").MapOut
-                    .ScaleFilter("trunc(iw/2)*2", "trunc(ih/2)*2").MapOut
-                    .SetSarFilter("1/1").MapOut
-                    .FpsFilter($"{config.Fps}").MapOut
-                    .FormatFilter(PixFmt.rgba).MapOut
                     .PadFilter($"{config.Size.Width * 4}", $"{config.Size.Height}").Position($"({config.Size.Width * 4}-iw)/2", $"({config.Size.Height}-ih)/2").Color(config.BackgroundColor).MapOut
                     .TrimFilter().Duration((c + 1) * (config.TransitionDuration + config.ImageDuration)).MapOut
                     .SetPtsFilter("PTS-STARTPTS").MapOut
 
 
-                    .RotateFilter($"if(between(t,{start.TotalSeconds},{end.TotalSeconds})," +
-                                        $"2*PI*t+if(eq(mod({c},2),0),1,-1)*{ANGLE_RANDOMNESS}*PI/180," +
-                                        $"if(eq(mod({c},2),0),1,-1)*{ANGLE_RANDOMNESS}*PI/180)")
+                    .RotateFilter($"if(" +
+                                        $"between(t,{start.TotalSeconds},{end.TotalSeconds})," +
+                                        $"{RPS}*2*PI*(t-{end.TotalSeconds})," +
+                                        $"0)" +
+                                    $"+if(eq(mod({c},2),0),1,-1)*{ANGLE_RANDOMNESS}*2*PI/360")
                         .OW($"{config.Size.Width * 4}").FillColor(config.BackgroundColor).MapOut
                     .OverlayFilterOn(lastOverLay,
                         $"if(gt(t,{start.TotalSeconds})," +
