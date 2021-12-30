3d9fefbb09dd2dc56513427d85676fe179c437a7
tqk2811 <tqk2811@gmail.com>
2021-12-31 02:01:01 +0700

    Filter: rework acrossover

--- FFmpegArgs.Filters/AudioFilters/AcrossoverFilter.cs
+++ FFmpegArgs.Filters/AudioFilters/AcrossoverFilter.cs
@@ -1,5 +1,23 @@
-﻿using FFmpegArgs.Cores.Maps;
+﻿/*
+acrossover AVOptions:
+  split             <string>     ..F.A...... set split frequencies (default "500")
+  order             <int>        ..F.A...... set filter order (from 0 to 9) (default 4th)
+     2nd             0            ..F.A...... 2nd order (12 dB/8ve)
+     4th             1            ..F.A...... 4th order (24 dB/8ve)
+     6th             2            ..F.A...... 6th order (36 dB/8ve)
+     8th             3            ..F.A...... 8th order (48 dB/8ve)
+     10th            4            ..F.A...... 10th order (60 dB/8ve)
+     12th            5            ..F.A...... 12th order (72 dB/8ve)
+     14th            6            ..F.A...... 14th order (84 dB/8ve)
+     16th            7            ..F.A...... 16th order (96 dB/8ve)
+     18th            8            ..F.A...... 18th order (108 dB/8ve)
+     20th            9            ..F.A...... 20th order (120 dB/8ve)
+  level             <float>      ..F.A...... set input gain (from 0 to 1) (default 1)
+  gain              <string>     ..F.A...... set output bands gain (default "1.f")
+ */
+using FFmpegArgs.Cores.Maps;
 using System;
+using System.Linq;
 
 namespace FFmpegArgs.Filters.AudioFilters
 {
@@ -32,12 +50,12 @@ namespace FFmpegArgs.Filters.AudioFilters
           => this.SetOptionRange("level", level, 0, 1);
 
         /// <summary>
-        /// Set output gain for each band. Default value is 1 for all bands.
+        /// set output bands gain (default "1.f")
         /// </summary>
         /// <param name="gains"></param>
         /// <returns></returns>
-        public AcrossoverFilter Gains(int gains)
-           => this.SetOptionRange("gains", gains, 0, int.MaxValue);
+        public AcrossoverFilter Gains(string gains)
+           => this.SetOption("gains", gains);
     }
 
     public static class AcrossoverFilterExtension
@@ -52,6 +70,7 @@ namespace FFmpegArgs.Filters.AudioFilters
         public static AcrossoverFilter AcrossoverFilter(this AudioMap audioMap, params int[] hzSplit)
         {
             if (hzSplit.Length == 0) throw new ArgumentNullException(nameof(hzSplit));
+            if (hzSplit.Any(x => x <= 0)) throw new InvalidOperationException("Hz requied > 0");
             return new AcrossoverFilter(hzSplit, audioMap);
         }
     }
