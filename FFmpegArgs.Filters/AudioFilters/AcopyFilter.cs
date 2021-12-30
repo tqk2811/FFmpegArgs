831ebb598ba088e02c89ef8399e969e3b9bcb3d2
tqk2811 <tqk2811@gmail.com>
2021-12-31 01:44:44 +0700

    Filter: remove acopy

--- FFmpegArgs.Filters/AudioFilters/AcopyFilter.cs
+++ FFmpegArgs.Filters/AudioFilters/AcopyFilter.cs
@@ -1,26 +1,26 @@
-﻿using FFmpegArgs.Cores.Maps;
+﻿//using FFmpegArgs.Cores.Maps;
 
-namespace FFmpegArgs.Filters.AudioFilters
-{
-    /// <summary>
-    /// ... acopy             A->A       Copy the input audio unchanged to the output.<br></br>
-    /// https://ffmpeg.org/ffmpeg-filters.html#acopy
-    /// </summary>
-    public class AcopyFilter : AudioToAudioFilter
-    {
-        internal AcopyFilter(AudioMap audioMap) : base("acopy", audioMap)
-        {
-            AddMapOut();
-        }
-    }
+//namespace FFmpegArgs.Filters.AudioFilters
+//{
+//    /// <summary>
+//    /// ... acopy             A->A       Copy the input audio unchanged to the output.<br></br>
+//    /// https://ffmpeg.org/ffmpeg-filters.html#acopy
+//    /// </summary>
+//    public class AcopyFilter : AudioToAudioFilter
+//    {
+//        internal AcopyFilter(AudioMap audioMap) : base("acopy", audioMap)
+//        {
+//            AddMapOut();
+//        }
+//    }
 
-    public static class AcopyFilterExtension
-    {
-        /// <summary>
-        /// Copy the input audio source unchanged to the output. This is mainly useful for testing purposes.
-        /// </summary>
-        /// <param name="audioMap"></param>
-        /// <returns></returns>
-        public static AcopyFilter AcopyFilter(this AudioMap audioMap) => new AcopyFilter(audioMap);
-    }
-}
+//    public static class AcopyFilterExtension
+//    {
+//        /// <summary>
+//        /// Copy the input audio source unchanged to the output. This is mainly useful for testing purposes.
+//        /// </summary>
+//        /// <param name="audioMap"></param>
+//        /// <returns></returns>
+//        public static AcopyFilter AcopyFilter(this AudioMap audioMap) => new AcopyFilter(audioMap);
+//    }
+//}
