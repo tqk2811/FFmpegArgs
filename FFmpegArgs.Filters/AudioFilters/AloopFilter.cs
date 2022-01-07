f456482e8bb5575691409b3efacc3387687e8cef
tqk2811 <tqk2811@gmail.com>
2022-01-07 15:04:32 +0700

    Filter: add Aloop, Loop

---
+++ FFmpegArgs.Filters/AudioFilters/AloopFilter.cs
@@ -0,0 +1,63 @@
+﻿/*
+aloop AVOptions:
+  loop              <int>        ..F.A...... number of loops (from -1 to INT_MAX) (default 0)
+  size              <int64>      ..F.A...... max number of samples to loop (from 0 to INT_MAX) (default 0)
+  start             <int64>      ..F.A...... set the loop start sample (from 0 to I64_MAX) (default 0)
+ */
+using FFmpegArgs.Cores.Filters;
+using FFmpegArgs.Cores.Maps;
+using FFmpegArgs.Filters.Enums;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace FFmpegArgs.Filters.AudioFilters
+{
+    /// <summary>
+    ///  ... aloop             A->A       Loop audio samples.<br></br>
+    /// https://ffmpeg.org/ffmpeg-filters.html#aloop
+    /// </summary>
+    public class AloopFilter : AudioToAudioFilter
+    {
+        internal AloopFilter(AudioMap audioMap) : base("aloop", audioMap)
+        {
+            AddMapOut();
+        }
+
+        /// <summary>
+        /// Set the number of loops. Setting this value to -1 will result in infinite loops. (from -1 to INT_MAX) (default 0)
+        /// </summary>
+        /// <param name="loop"></param>
+        /// <returns></returns>
+        public AloopFilter Loop(int loop) => this.SetOptionRange("loop", loop, -1, INT_MAX);
+
+        /// <summary>
+        /// Set maximal number of samples. (from 0 to INT_MAX) (default 0)
+        /// </summary>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public AloopFilter Size(long size) => this.SetOptionRange("size", size, 0, INT_MAX);
+
+        /// <summary>
+        /// Set first sample of loop. (from 0 to I64_MAX) (default 0)
+        /// </summary>
+        /// <param name="start"></param>
+        /// <returns></returns>
+        public AloopFilter Start(long start) => this.SetOptionRange("start", start, 0, I64_MAX);
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class AloopFilterExtensions
+    {
+        /// <summary>
+        /// Loop audio samples.
+        /// </summary>
+        public static AloopFilter AloopFilter(this AudioMap audioMap)
+          => new AloopFilter(audioMap);
+    }
+}
---
+++ FFmpegArgs.Filters/VideoFilters/LoopFilter.cs
@@ -0,0 +1,63 @@
+﻿/*
+loop AVOptions:
+  loop              <int>        ..FV....... number of loops (from -1 to INT_MAX) (default 0)
+  size              <int64>      ..FV....... max number of frames to loop (from 0 to 32767) (default 0)
+  start             <int64>      ..FV....... set the loop start frame (from 0 to I64_MAX) (default 0)
+ */
+using FFmpegArgs.Cores.Filters;
+using FFmpegArgs.Cores.Maps;
+using FFmpegArgs.Filters.Enums;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace FFmpegArgs.Filters.VideoFilters
+{
+    /// <summary>
+    ///  ... loop              V->V       Loop video frames.<br></br>
+    /// https://ffmpeg.org/ffmpeg-filters.html#loop
+    /// </summary>
+    public class LoopFilter : ImageToImageFilter
+    {
+        internal LoopFilter(ImageMap imageMap) : base("loop", imageMap)
+        {
+            AddMapOut();
+        }
+
+        /// <summary>
+        /// Set the number of loops. Setting this value to -1 will result in infinite loops. (from -1 to INT_MAX) (default 0)
+        /// </summary>
+        /// <param name="loop"></param>
+        /// <returns></returns>
+        public LoopFilter Loop(int loop) => this.SetOptionRange("loop", loop, -1, INT_MAX);
+
+        /// <summary>
+        /// Set maximal size in number of frames. (from 0 to 32767) (default 0)
+        /// </summary>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public LoopFilter Size(long size) => this.SetOptionRange("size", size, 0, 32767);
+
+        /// <summary>
+        /// Set first frame of loop. (from 0 to I64_MAX) (default 0)
+        /// </summary>
+        /// <param name="start"></param>
+        /// <returns></returns>
+        public LoopFilter Start(long start) => this.SetOptionRange("start", start, 0, I64_MAX);
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class LoopFilterExtensions
+    {
+        /// <summary>
+        /// Loop video frames.
+        /// </summary>
+        public static LoopFilter LoopFilter(this ImageMap imageMap)
+          => new LoopFilter(imageMap);
+    }
+}
