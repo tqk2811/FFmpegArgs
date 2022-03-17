f79dbfca9705d996e93a70bcc69d2c13168eb753
tqk2811 <tqk2811@gmail.com>
2022-03-18 01:14:05 +0700

    Filter: add asuperstop, atempo, axcorrelate, bandpass, bandreject. add doc atrim

---
+++ FFmpegArgs.Filters/AudioFilters/AsuperstopFilter.cs
@@ -0,0 +1,64 @@
+﻿/*
+asuperstop AVOptions:
+  centerf           <double>     ..F.A....T. set center frequency (from 2 to 999999) (default 1000)
+  order             <int>        ..F.A....T. set filter order (from 4 to 20) (default 4)
+  qfactor           <double>     ..F.A....T. set Q-factor (from 0.01 to 100) (default 1)
+  level             <double>     ..F.A....T. set input level (from 0 to 2) (default 1)
+*/
+namespace FFmpegArgs.Filters.AudioFilters
+{
+    /// <summary>
+    /// TSC asuperstop        A->A       Apply high order Butterworth band-stop filter.<br></br>
+    /// https://ffmpeg.org/ffmpeg-filters.html#asuperstop
+    /// </summary>
+    public class AsuperstopFilter : AudioToAudioFilter, ITimelineSupport, ISliceThreading, ICommandSupport
+    {
+        internal AsuperstopFilter(AudioMap audioMap) : base("asuperstop", audioMap)
+        {
+            AddMapOut();
+        }
+
+        /// <summary>
+        /// Set center frequency in Hertz. Allowed range is 2 to 999999. Default value is 1000.
+        /// </summary>
+        /// <param name="centerf"></param>
+        /// <returns></returns>
+        public AsuperstopFilter Centerf(double centerf)
+            => this.SetOptionRange("centerf", centerf, 2, 999999);
+
+        /// <summary>
+        /// Set filter order. Available values are from 4 to 20. Default value is 4.
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        public AsuperstopFilter Order(int order)
+            => this.SetOptionRange("order", order, 4, 20);
+
+        /// <summary>
+        /// Set Q-factor. Allowed range is from 0.01 to 100. Default value is 1.
+        /// </summary>
+        /// <param name="qfactor"></param>
+        /// <returns></returns>
+        public AsuperstopFilter Qfactor(double qfactor)
+            => this.SetOptionRange("qfactor", qfactor, 0.01, 100);
+
+        /// <summary>
+        /// Set input gain level. Allowed range is from 0 to 2. Default value is 1.
+        /// </summary>
+        /// <param name="level"></param>
+        /// <returns></returns>
+        public AsuperstopFilter Level(double level)
+            => this.SetOptionRange("level", level, 0, 2);
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class AsuperstopFilterExtensions
+    {
+        /// <summary>
+        /// Apply high order Butterworth band-stop filter.
+        /// </summary>
+        public static AsuperstopFilter AsuperstopFilter(this AudioMap audioMap)
+          => new AsuperstopFilter(audioMap);
+    }
+}
---
+++ FFmpegArgs.Filters/AudioFilters/BandrejectFilter.cs
@@ -0,0 +1,214 @@
+﻿/*
+bandreject AVOptions:
+  frequency         <double>     ..F.A....T. set central frequency (from 0 to 999999) (default 3000)
+  f                 <double>     ..F.A....T. set central frequency (from 0 to 999999) (default 3000)
+  width_type        <int>        ..F.A....T. set filter-width type (from 1 to 5) (default q)
+     h               1            ..F.A....T. Hz
+     q               3            ..F.A....T. Q-Factor
+     o               2            ..F.A....T. octave
+     s               4            ..F.A....T. slope
+     k               5            ..F.A....T. kHz
+  t                 <int>        ..F.A....T. set filter-width type (from 1 to 5) (default q)
+     h               1            ..F.A....T. Hz
+     q               3            ..F.A....T. Q-Factor
+     o               2            ..F.A....T. octave
+     s               4            ..F.A....T. slope
+     k               5            ..F.A....T. kHz
+  width             <double>     ..F.A....T. set band-width (from 0 to 99999) (default 0.5)
+  w                 <double>     ..F.A....T. set band-width (from 0 to 99999) (default 0.5)
+  mix               <double>     ..F.A....T. set mix (from 0 to 1) (default 1)
+  m                 <double>     ..F.A....T. set mix (from 0 to 1) (default 1)
+  channels          <channel_layout> ..F.A....T. set channels to filter (default 0xffffffffffffffff)
+  c                 <channel_layout> ..F.A....T. set channels to filter (default 0xffffffffffffffff)
+  normalize         <boolean>    ..F.A....T. normalize coefficients (default false)
+  n                 <boolean>    ..F.A....T. normalize coefficients (default false)
+  transform         <int>        ..F.A...... set transform type (from 0 to 3) (default di)
+     di              0            ..F.A...... direct form I
+     dii             1            ..F.A...... direct form II
+     tdii            2            ..F.A...... transposed direct form II
+     latt            3            ..F.A...... lattice-ladder form
+  a                 <int>        ..F.A...... set transform type (from 0 to 3) (default di)
+     di              0            ..F.A...... direct form I
+     dii             1            ..F.A...... direct form II
+     tdii            2            ..F.A...... transposed direct form II
+     latt            3            ..F.A...... lattice-ladder form
+  precision         <int>        ..F.A...... set filtering precision (from -1 to 3) (default auto)
+     auto            -1           ..F.A...... automatic
+     s16             0            ..F.A...... signed 16-bit
+     s32             1            ..F.A...... signed 32-bit
+     f32             2            ..F.A...... floating-point single
+     f64             3            ..F.A...... floating-point double
+  r                 <int>        ..F.A...... set filtering precision (from -1 to 3) (default auto)
+     auto            -1           ..F.A...... automatic
+     s16             0            ..F.A...... signed 16-bit
+     s32             1            ..F.A...... signed 32-bit
+     f32             2            ..F.A...... floating-point single
+     f64             3            ..F.A...... floating-point double
+*/
+namespace FFmpegArgs.Filters.AudioFilters
+{
+    /// <summary>
+    /// TSC bandreject        A->A       Apply a two-pole Butterworth band-reject filter.<br></br>
+    /// https://ffmpeg.org/ffmpeg-filters.html#bandreject
+    /// </summary>
+    public class BandrejectFilter : AudioToAudioFilter, ITimelineSupport, ISliceThreading, ICommandSupport
+    {
+        internal BandrejectFilter(AudioMap audioMap) : base("bandreject", audioMap)
+        {
+            AddMapOut();
+        }
+
+        /// <summary>
+        /// set central frequency (from 0 to 999999) (default 3000)
+        /// </summary>
+        /// <param name="frequency"></param>
+        /// <returns></returns>
+        public BandrejectFilter Frequency(double frequency)
+            => this.SetOptionRange("f", frequency, 0, 999999);
+
+        /// <summary>
+        /// set filter-width type (from 1 to 5) (default q)
+        /// </summary>
+        /// <param name="widthType"></param>
+        /// <returns></returns>
+        public BandrejectFilter WidthType(BandrejectWidthType widthType)
+            => this.SetOption("t", widthType);
+
+        /// <summary>
+        /// set band-width (from 0 to 99999) (default 0.5)
+        /// </summary>
+        /// <param name="width"></param>
+        /// <returns></returns>
+        public BandrejectFilter Width(double width)
+            => this.SetOptionRange("w", width, 0, 99999);
+
+        /// <summary>
+        /// How much to use filtered signal in output. Default is 1. Range is between 0 and 1.
+        /// </summary>
+        /// <param name="mix"></param>
+        /// <returns></returns>
+        public BandrejectFilter Mix(double mix)
+            => this.SetOptionRange("m", mix, 0, 1);
+
+        /// <summary>
+        /// set channels to filter (default 0xffffffffffffffff)
+        /// </summary>
+        /// <param name="channels"></param>
+        /// <returns></returns>
+        public BandrejectFilter Channels(ChannelLayout channels)
+            => this.SetOption("c", channels.GetEnumAttribute<NameAttribute>().Name);
+
+        /// <summary>
+        /// Normalize biquad coefficients, by default is disabled. Enabling it will normalize magnitude response at DC to 0dB.
+        /// </summary>
+        /// <param name="normalize"></param>
+        /// <returns></returns>
+        public BandrejectFilter Normalize(bool normalize)
+            => this.SetOption("n", normalize.ToFFmpegFlag());
+        /// <summary>
+        /// Set transform type of IIR filter. (default di)
+        /// </summary>
+        /// <param name="transform"></param>
+        /// <returns></returns>
+        public BandrejectFilter Transform(BandrejectTransform transform)
+            => this.SetOption("a", transform);
+        /// <summary>
+        /// set filtering precision (default auto)
+        /// </summary>
+        /// <param name="precision"></param>
+        /// <returns></returns>
+        public BandrejectFilter Precision(BandrejectPrecision precision)
+            => this.SetOption("r", precision);
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class BandrejectFilterExtensions
+    {
+        /// <summary>
+        /// Apply a two-pole Butterworth band-reject filter with central frequency frequency, and (3dB-point) band-width width. The filter roll off at 6dB per octave (20dB per decade).
+        /// </summary>
+        public static BandrejectFilter BandrejectFilter(this AudioMap audioMap)
+          => new BandrejectFilter(audioMap);
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum BandrejectWidthType
+    {
+        /// <summary>
+        /// Hz
+        /// </summary>
+        h,
+        /// <summary>
+        /// Q-Factor
+        /// </summary>
+        q,
+        /// <summary>
+        /// octave
+        /// </summary>
+        o,
+        /// <summary>
+        /// slope
+        /// </summary>
+        s,
+        /// <summary>
+        /// kHz
+        /// </summary>
+        k,
+
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum BandrejectTransform
+    {
+        /// <summary>
+        /// direct form I
+        /// </summary>
+        di,
+        /// <summary>
+        /// direct form II
+        /// </summary>
+        dii,
+        /// <summary>
+        /// transposed direct form II
+        /// </summary>
+        tdii,
+        /// <summary>
+        /// lattice-ladder form
+        /// </summary>
+        latt
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum BandrejectPrecision
+    {
+        /// <summary>
+        /// automatic
+        /// </summary>
+        auto,
+        /// <summary>
+        /// signed 16-bit
+        /// </summary>
+        s16,
+        /// <summary>
+        /// signed 32-bit
+        /// </summary>
+        s32,
+        /// <summary>
+        /// floating-point single
+        /// </summary>
+        f32,
+        /// <summary>
+        /// floating-point double
+        /// </summary>
+        f64
+
+    }
+}
