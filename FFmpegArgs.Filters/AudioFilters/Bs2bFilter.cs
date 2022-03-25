2305c33027c8ba3778122a09e885fcd1b90959d6
tqk2811 <tqk2811@gmail.com>
2022-03-25 10:41:51 +0700

    Filter: add channelmap, channelsplit, chorus

---
+++ FFmpegArgs.Filters/AudioFilters/Bs2bFilter.cs
@@ -0,0 +1,31 @@
+﻿///*
+
+//*/
+//namespace FFmpegArgs.Filters.AudioFilters
+//{
+//    /// <summary>
+//    /// <br></br>
+//    /// https://ffmpeg.org/ffmpeg-filters.html#bs2b
+//    /// </summary>
+//    public class Bs2bFilter : AudioToAudioFilter
+//    {
+//        internal Bs2bFilter(AudioMap audioMap) : base("bs2b", audioMap)
+//        {
+//            AddMapOut();
+//        }
+
+
+
+//    }
+//    /// <summary>
+//    /// 
+//    /// </summary>
+//    public static class Bs2bFilterExtensions
+//    {
+//        /// <summary>
+//        /// Bauer stereo to binaural transformation, which improves headphone listening of stereo audio records.
+//        /// </summary>
+//        public static Bs2bFilter Bs2bFilter(this AudioMap audioMap)
+//          => new Bs2bFilter(audioMap);
+//    }
+//}
