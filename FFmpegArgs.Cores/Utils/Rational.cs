3cfd513bf68312a9e64d153172776980e53a7f61
tqk2811 <tqk2811@gmail.com>
2023-04-25 18:03:25 +0700

    Core: Rational remove implicit from float, double, decimal

--- FFmpegArgs.Cores/Utils/Rational.cs
+++ FFmpegArgs.Cores/Utils/Rational.cs
@@ -1,4 +1,6 @@
-﻿namespace FFmpegArgs.Cores.Utils
+﻿using System.Text;
+
+namespace FFmpegArgs.Cores.Utils
 {
     /// <summary>
     /// 
@@ -9,17 +11,17 @@
         /// 
         /// </summary>
         public int Numerator { get; set; }
-        
+
         /// <summary>
         /// 
         /// </summary>
         public int Denominator { get; set; }
-        
+
         /// <summary>
         /// 
         /// </summary>
         public Rational() { }
-        
+
         /// <summary>
         /// 
         /// </summary>
@@ -30,7 +32,7 @@
             this.Numerator = num;
             this.Denominator = den;
         }
-        
+
         /// <summary>
         /// Create with den = 1
         /// </summary>
@@ -47,7 +49,7 @@
         /// <returns></returns>
         public static Rational Create(int num, int den)
             => new Rational(num, den);
-        
+
         /// <summary>
         /// 
         /// </summary>
@@ -67,59 +69,47 @@
         /// </summary>
         /// <returns></returns>
         public string ToStringColon() => $"{Numerator}:{Denominator}";
-        
+
         /// <summary>
         /// Num/Den
         /// </summary>
         /// <returns></returns>
         public string ToStringSlash() => $"{Numerator}/{Denominator}";
-        
+
         /// <summary>
         /// Num/Den
         /// </summary>
         /// <returns></returns>
         public override string ToString() => ToStringSlash();
-        
+
         /// <summary>
         /// 
         /// </summary>
         /// <param name="num"></param>
         public static implicit operator Rational(int num) => Create(num);
-        
+
         /// <summary>
         /// 
         /// </summary>
         /// <param name="num"></param>
         public static implicit operator Rational(long num) => Create((int)num);
-        
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="num"></param>
-        public static implicit operator Rational(float num) => Create((int)num);
-        
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="num"></param>
-        public static implicit operator Rational(double num) => Create((int)num);
-        
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="num"></param>
-        public static implicit operator Rational(decimal num) => Create((int)num);
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="r"></param>
         public static explicit operator double(Rational r) => (double)r.Numerator / r.Denominator;
-        
+
         /// <summary>
         /// 
         /// </summary>
         /// <param name="r"></param>
         public static explicit operator float(Rational r) => (float)r.Numerator / r.Denominator;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="r"></param>
+        public static explicit operator decimal(Rational r) => (decimal)r.Numerator / r.Denominator;
     }
 }
