8b6f1581de007e53ff2a4727a4a8dc0d5a1fbfdf
tqk2811 <tqk2811@gmail.com>
2023-05-05 16:13:53 +0700

    Test: add RationalTest

---
+++ FFmpegArgs.Test/FeatureTest/RationalTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFmpegArgs.Test.FeatureTest
+{
+    [TestClass]
+    public class RationalTest
+    {
+        [TestMethod]
+        public void TestParse()
+        {
+            Rational rational = null;
+            Assert.IsTrue(Rational.TryParse("2000:10", out rational));
+            Assert.IsTrue(Rational.TryParse("2000.04635:10", out rational));
+            Assert.IsTrue(Rational.TryParse("2000/0", out rational));
+            Assert.IsTrue(Rational.TryParse("2000.5261/10", out rational));
+        }
+    }
+}
