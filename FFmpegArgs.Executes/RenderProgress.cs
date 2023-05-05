b8e21b78c469082cd090d47a73033c826977798e
tqk2811 <tqk2811@gmail.com>
2023-05-05 16:25:38 +0700

    Executes: Fix parse duration & exception

--- FFmpegArgs.Executes/RenderProgress.cs
+++ FFmpegArgs.Executes/RenderProgress.cs
@@ -6,8 +6,8 @@
     public class RenderProgress
     {
         static readonly Regex regex
-            = new Regex(@"frame= *(\d+) fps= *([0-9.]+).*?size= *(N\/A|[0-9.]+kB).*?time= *(\d{2}:\d{2}:\d{2}\.\d+).*?bitrate= *(N\/A|[0-9.]+kbits\/s).*?speed= *([0-9.]+)x");
-        
+            = new Regex(@"frame= *(\d+) fps= *([0-9.]+).*?size= *(N\/A|[0-9.]+kB).*?time= *(\d+:\d{1,2}:\d{1,2}\.\d+).*?bitrate= *(N\/A|[0-9.]+kbits\/s).*?speed= *([0-9.]+)x");
+
         /// <summary>
         /// 
         /// </summary>
@@ -20,19 +20,63 @@
                 Match match = regex.Match(progress);
                 if (match.Success && match.Groups.Count == 7)
                 {
-                    return new RenderProgress
+                    try
                     {
-                        Frame = int.Parse(match.Groups[1].Value),
-                        Fps = double.Parse(match.Groups[2].Value),
-                        Size = match.Groups[3].Value.Equals("N/A") ? double.NaN : double.Parse(match.Groups[3].Value.Replace("kB", string.Empty)),
-                        Time = TimeSpan.Parse(match.Groups[4].Value),
-                        Bitrate = match.Groups[5].Value.Equals("N/A") ? double.NaN : double.Parse(match.Groups[5].Value.Replace("kbits/s", string.Empty)),
-                        Speed = float.Parse(match.Groups[6].Value)
-                    };
+                        return new RenderProgress
+                        {
+                            Frame = int.Parse(match.Groups[1].Value),
+                            Fps = double.Parse(match.Groups[2].Value),
+                            Size = match.Groups[3].Value.Equals("N/A") ? double.NaN : double.Parse(match.Groups[3].Value.Replace("kB", string.Empty)),
+                            Time = ParseDuration(match.Groups[4].Value),
+                            Bitrate = match.Groups[5].Value.Equals("N/A") ? double.NaN : double.Parse(match.Groups[5].Value.Replace("kbits/s", string.Empty)),
+                            Speed = float.Parse(match.Groups[6].Value)
+                        };
+                    }
+                    catch (Exception)
+                    {
+
+                    }
                 }
             }
             return null;
         }
+        private static readonly Regex DurationRegex = new(@"^(\d+):(\d{1,2}):(\d{1,2})\.(\d{1,3})", RegexOptions.Compiled);
+        //https://github.com/rosenbjerg/FFMpegCore/blob/943662aa15e8cf73dce0b1a8b93ffe8e8bae274b/FFMpegCore/FFProbe/MediaAnalysis.cs#L166
+        private static TimeSpan ParseDuration(string duration)
+        {
+            if (!string.IsNullOrEmpty(duration))
+            {
+                var match = DurationRegex.Match(duration);
+                if (match.Success)
+                {
+                    // ffmpeg may provide < 3-digit number of milliseconds (omitting trailing zeros), which won't simply parse correctly
+                    // e.g. 00:12:02.11 -> 12 minutes 2 seconds and 110 milliseconds
+                    var millisecondsPart = match.Groups[4].Value;
+                    if (millisecondsPart.Length < 3)
+                    {
+                        millisecondsPart = millisecondsPart.PadRight(3, '0');
+                    }
+
+                    var hours = int.Parse(match.Groups[1].Value);
+                    var minutes = int.Parse(match.Groups[2].Value);
+                    var seconds = int.Parse(match.Groups[3].Value);
+                    var milliseconds = int.Parse(millisecondsPart);
+                    return new TimeSpan(0, hours, minutes, seconds, milliseconds);
+                }
+                else
+                {
+                    return TimeSpan.Zero;
+                }
+            }
+            else
+            {
+                return TimeSpan.Zero;
+            }
+        }
+
+
+
+
         internal RenderProgress() { }
         /// <summary>
         /// Frame rendered
