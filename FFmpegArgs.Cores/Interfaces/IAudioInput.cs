4499551f4bc61563a645e1e3d4b498b8fd2f29c7
tqk2811 <tqk2811@gmail.com>
2023-07-27 17:59:29 +0700

    Cores: add IAudioInput, IImageInput

---
+++ FFmpegArgs.Cores/Interfaces/IAudioInput.cs
@@ -0,0 +1,18 @@
+﻿namespace FFmpegArgs.Cores.Interfaces
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IAudioInput : IAudio, IInput
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        IEnumerable<AudioInputAVStream> AudioInputAVStreams { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        AudioInputAVStream AudioInputAVStream { get; }
+    }
+}
---
+++ FFmpegArgs.Cores/Interfaces/IImageInput.cs
@@ -0,0 +1,18 @@
+﻿namespace FFmpegArgs.Cores.Interfaces
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IImageInput : IImage, IInput
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        IEnumerable<ImageInputAVStream> ImageInputAVStreams { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        ImageInputAVStream ImageInputAVStream { get; }
+    }
+}
