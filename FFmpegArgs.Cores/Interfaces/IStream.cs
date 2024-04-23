df9145ac23ce3f87232e139d1c7ecc890ff9251d
tqk2811 <tqk2811@gmail.com>
2024-04-23 15:22:52 +0700

    add stream interface

---
+++ FFmpegArgs.Cores/Interfaces/IStream.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFmpegArgs.Cores.Interfaces
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IStream
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        int StreamIndex { get; }
+    }
+}
