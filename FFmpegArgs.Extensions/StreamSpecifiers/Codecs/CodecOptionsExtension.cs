4b6cd463ba577ae180f03a8a36c59f1509c73fb5
tqk2811 <tqk2811@gmail.com>
2024-04-23 16:58:25 +0700

    update interface codec extension

--- FFmpegArgs.Extensions/StreamSpecifiers/Codecs/CodecOptionsExtension.cs
+++ FFmpegArgs.Extensions/StreamSpecifiers/Codecs/CodecOptionsExtension.cs
@@ -5,15 +5,26 @@
     /// </summary>
     public static class CodecOptionsExtension
     {
+        /// <summary>
+        /// Select an encoder (when used before an output file) or a decoder (when used before an input file) for one or more streams. codec is the name of a decoder/encoder or a special value copy (output only) to indicate that the stream is not to be re-encoded.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="t"></param>
+        /// <param name="codec"></param>
+        /// <returns></returns>
+        public static T Codec<T>(this T t, string codec) where T : BaseAVStream, ICodec // (input/output,per-stream)
+           => t.SetOption("-c", codec);
+
+
         #region audio,video
 
-        internal static T B<T>(this T t, int bitrate) where T : OutputAVStream//encoding,audio,video
+        internal static T B<T>(this T t, int bitrate) where T : OutputAVStream, ICodec, ICodecEncoding//encoding,audio,video
             => t.SetOptionRange("-b", bitrate, int.MinValue, int.MaxValue);
-        internal static T Maxrate<T>(this T t, int maxrate) where T : OutputAVStream//encoding,audio,video
+        internal static T Maxrate<T>(this T t, int maxrate) where T : OutputAVStream, ICodec, ICodecEncoding//encoding,audio,video
             => t.SetOptionRange("-maxrate", maxrate, int.MinValue, int.MaxValue);
-        internal static T Minrate<T>(this T t, int minrate) where T : OutputAVStream//encoding,audio,video
+        internal static T Minrate<T>(this T t, int minrate) where T : OutputAVStream, ICodec, ICodecEncoding//encoding,audio,video
             => t.SetOptionRange("-minrate", minrate, int.MinValue, int.MaxValue);
-        internal static T Bufsize<T>(this T t, int bufsize) where T : OutputAVStream//encoding,audio,video
+        internal static T Bufsize<T>(this T t, int bufsize) where T : OutputAVStream, ICodec, ICodecEncoding//encoding,audio,video
             => t.SetOptionRange("-bufsize", bufsize, int.MinValue, int.MaxValue);
 
 
@@ -32,7 +43,7 @@
         /// <param name="t"></param>
         /// <param name="frame_number"></param>
         /// <returns></returns>
-        public static T FrameNumber<T>(this T t, int frame_number) where T : BaseAVStream
+        public static T FrameNumber<T>(this T t, int frame_number) where T : BaseAVStream, ICodec
             => t.SetOptionRange("-frame_number", frame_number, int.MinValue, int.MaxValue);
 
         /// <summary>
@@ -42,7 +53,7 @@
         /// <param name="t"></param>
         /// <param name="delay"></param>
         /// <returns></returns>
-        public static T Delay<T>(this T t, int delay) where T : BaseAVStream
+        public static T Delay<T>(this T t, int delay) where T : BaseAVStream, ICodec
             => t.SetOptionRange("-delay", delay, int.MinValue, int.MaxValue);
 
         /// <summary>
@@ -52,7 +63,7 @@
         /// <param name="t"></param>
         /// <param name="codec_tag"></param>
         /// <returns></returns>
-        public static T CodecTag<T>(this T t, int codec_tag) where T : BaseAVStream
+        public static T CodecTag<T>(this T t, int codec_tag) where T : BaseAVStream, ICodec
             => t.SetOptionRange("-codec_tag", codec_tag, int.MinValue, int.MaxValue);
 
         /// <summary>
@@ -63,7 +74,7 @@
         /// <param name="t"></param>
         /// <param name="has_b_frames"></param>
         /// <returns></returns>
-        public static T HasBFrames<T>(this T t, int has_b_frames) where T : BaseAVStream
+        public static T HasBFrames<T>(this T t, int has_b_frames) where T : BaseAVStream, ICodec
             => t.SetOptionRange("-has_b_frames", has_b_frames, int.MinValue, int.MaxValue);
 
         /// <summary>
@@ -74,7 +85,7 @@
         /// <param name="t"></param>
         /// <param name="has_b_frames"></param>
         /// <returns></returns>
-        public static T BlockAlign<T>(this T t, int has_b_frames) where T : BaseAVStream
+        public static T BlockAlign<T>(this T t, int has_b_frames) where T : BaseAVStream, ICodec
             => t.SetOptionRange("-block_align", has_b_frames, int.MinValue, int.MaxValue);
 
         /// <summary>
@@ -84,7 +95,7 @@
         /// <param name="t"></param>
         /// <param name="has_b_frames"></param>
         /// <returns></returns>
-        public static T RcOverrideCount<T>(this T t, int has_b_frames) where T : BaseAVStream
+        public static T RcOverrideCount<T>(this T t, int has_b_frames) where T : BaseAVStream, ICodec
             => t.SetOptionRange("-rc_override_count", has_b_frames, int.MinValue, int.MaxValue);
 
         /// <summary>
@@ -94,7 +105,7 @@
         /// <param name="t"></param>
         /// <param name="slice_count"></param>
         /// <returns></returns>
-        public static T SliceCount<T>(this T t, int slice_count) where T : BaseAVStream
+        public static T SliceCount<T>(this T t, int slice_count) where T : BaseAVStream, ICodec
             => t.SetOptionRange("-slice_count", slice_count, int.MinValue, int.MaxValue);
 
         /// <summary>
@@ -104,13 +115,13 @@
         /// <param name="t"></param>
         /// <param name="bits_per_coded_sample"></param>
         /// <returns></returns>
-        public static T BitsPerCodedSample<T>(this T t, int bits_per_coded_sample) where T : BaseAVStream
+        public static T BitsPerCodedSample<T>(this T t, int bits_per_coded_sample) where T : BaseAVStream, ICodec
             => t.SetOptionRange("-bits_per_coded_sample", bits_per_coded_sample, int.MinValue, int.MaxValue);
 
         //log_level_offset 
 
         //pkt_timebase 
-        
+
         #endregion
 
     }
