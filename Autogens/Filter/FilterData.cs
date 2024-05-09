4f098c8b78de5f43c11e1ac8fd933f093d759688
tqk2811 <tqk2811@gmail.com>
2024-05-09 20:15:28 +0700

    note about EDFVASXRBTP

--- Autogens/Filter/FilterData.cs
+++ Autogens/Filter/FilterData.cs
@@ -3,6 +3,21 @@ namespace Autogens.Filter
 {
     internal class FilterData
     {
+        /// <summary>
+        /// libavutil\opt.h
+        /// EDFVASXRBTP
+        /// E               AV_OPT_FLAG_ENCODING_PARAM
+        ///  D              AV_OPT_FLAG_DECODING_PARAM
+        ///   F             AV_OPT_FLAG_FILTERING_PARAM
+        ///    V            AV_OPT_FLAG_VIDEO_PARAM
+        ///     A           AV_OPT_FLAG_AUDIO_PARAM
+        ///      S          AV_OPT_FLAG_SUBTITLE_PARAM
+        ///       X         AV_OPT_FLAG_EXPORT
+        ///        R        AV_OPT_FLAG_READONLY
+        ///         B       AV_OPT_FLAG_BSF_PARAM
+        ///          T      AV_OPT_FLAG_RUNTIME_PARAM
+        ///           P     AV_OPT_FLAG_DEPRECATED
+        /// </summary>
         internal static Regex regex_DocLineMethod { get; } = new Regex("^([0-9A-z-+<>_-]+) +(|[0-9A-z<>-]+) +([.EDFVASXRBTP]{11})(.*?|)$");
         static Regex regex_DocLineMethodFromTo { get; } = new Regex("from ([0-9A-Z-+._e]+) to ([0-9A-Z-+._e]+)");
         internal FilterData(DocLine function)
