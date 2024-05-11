ad0d6c5eaaa597a890b08da4b942f348b15419ce
tqk2811 <tqk2811@gmail.com>
2024-05-11 16:20:03 +0700

    add InvalidException

---
+++ FFmpegArgs.Cores/Exceptions/InvalidException.cs
@@ -0,0 +1,16 @@
+﻿namespace FFmpegArgs.Cores.Exceptions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class InvalidException : Exception
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        public InvalidException(string message) : base(message)
+        {
+        }
+    }
+}
