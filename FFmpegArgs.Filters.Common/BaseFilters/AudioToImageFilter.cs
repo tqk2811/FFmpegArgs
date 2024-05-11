ad4144d9e8bca826ac8fdce93774a90d79f4f0ac
tqk2811 <tqk2811@gmail.com>
2024-05-11 15:02:01 +0700

    add AudioToImageFilter

---
+++ FFmpegArgs.Filters.Common/BaseFilters/AudioToImageFilter.cs
@@ -0,0 +1,31 @@
+﻿namespace FFmpegArgs.Filters.BaseFilters
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AudioToImageFilter : BaseFilter<AudioMap, ImageMap>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filterName"></param>
+        /// <param name="mapsIn"></param>
+        protected AudioToImageFilter(string filterName, params AudioMap[] mapsIn) : base(filterName, mapsIn)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override void AddMapOut()
+            => _mapsOut.Add(new ImageMap(FilterGraph, $"f_{FilterIndex}"));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index"></param>
+        protected override void AddMapOut(int index)
+            => _mapsOut.Add(new ImageMap(FilterGraph, $"f_{FilterIndex}_{index}"));
+    }
+}
