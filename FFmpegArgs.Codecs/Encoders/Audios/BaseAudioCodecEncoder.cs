12e052d6be3d56d2baf091660e742fbda3632739
tqk2811 <tqk2811@gmail.com>
2024-05-11 17:36:12 +0700

    tmp

---
+++ FFmpegArgs.Codecs/Encoders/Audios/BaseAudioCodecEncoder.cs
@@ -0,0 +1,25 @@
+﻿using FFmpegArgs.Cores.Interfaces;
+using FFmpegArgs.Cores.Streams;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFmpegArgs.Codecs.Encoders.Audios
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class BaseAudioCodecEncoder : BaseCodecEncoder, IAudioCodec
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="codecName"></param>
+        /// <param name="stream"></param>
+        protected BaseAudioCodecEncoder(string codecName, AudioOutputAVStream stream) : base(codecName, stream)
+        {
+        }
+    }
+}
