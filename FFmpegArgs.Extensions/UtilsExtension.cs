8d45ea7afb2d808962b9e4a5cbd72aa19172e76e
tqk2811 <tqk2811@gmail.com>
2025-04-09 16:15:42 +0700

    move some extension to core

--- FFmpegArgs.Extensions/UtilsExtension.cs
+++ FFmpegArgs.Extensions/UtilsExtension.cs
@@ -59,43 +59,5 @@
             if (input % 2 != 0) throw new InvalidOperationException("required input even");
             return input;
         }
-
-
-
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="flag"></param>
-        /// <returns></returns>
-        public static string ToFFmpegFlag(this bool flag)
-            => flag ? "1" : "0";
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="color"></param>
-        /// <returns></returns>
-        public static string ToHexStringRGB(this Color color)
-            => $"0x{color.R:X2}{color.G:X2}{color.B:X2}";
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="color"></param>
-        /// <returns></returns>
-        public static string ToHexStringRGBA(this Color color)
-            => $"0x{color.R:X2}{color.G:X2}{color.B:X2}{color.A:X2}";
-
-        /// <summary>
-        /// <see href="https://ffmpeg.org/ffmpeg-utils.html#date-syntax"/>
-        /// </summary>
-        /// <param name="dateTime"></param>
-        /// <returns></returns>
-        public static string ToFFmpegDate(this DateTime dateTime)
-            => dateTime.ToString("yyyy-MM-dd HH:mm:ss");
-
-
-
     }
 }
