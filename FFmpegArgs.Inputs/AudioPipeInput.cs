ae1d40ba632dc2a143da0b666fb64d7fc32580c1
tqk2811 <tqk2811@gmail.com>
2025-10-03 15:28:32 +0700

    add AudioPipeInput

---
+++ FFmpegArgs.Inputs/AudioPipeInput.cs
@@ -0,0 +1,34 @@
+﻿namespace FFmpegArgs.Inputs
+{
+    public class AudioPipeInput : AudioInput
+    {
+        public AudioPipeInput(
+            Stream stream,
+            DemuxingFileFormat format,
+            int audioStreamCount = 1
+            )
+            : base(audioStreamCount)
+        {
+            this.PipeStream = stream ?? throw new ArgumentNullException(nameof(stream));
+            if (!stream.CanRead) throw new InvalidOperationException("input stream.CanRead is required");
+            this.Format(format);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override Stream PipeStream { get; }
+
+        public override string ToString()
+        {
+            List<string> args = new List<string>()
+            {
+                GetFlagArgs(),
+                GetOptionArgs(),
+                GetAVStreamArg(),
+                $"-i pipe:{StdIn}"
+            };
+            return $"{string.Join(" ", args.Where(x => !string.IsNullOrWhiteSpace(x)))}";
+        }
+    }
+}
