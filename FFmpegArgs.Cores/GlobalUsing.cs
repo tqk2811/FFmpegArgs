73c34aaeca18bcf95d10adeb0bd04a85e40f1a73
tqk2811 <tqk2811@gmail.com>
2025-10-03 15:05:31 +0700

    global using CodeAnalysis

--- FFmpegArgs.Cores/GlobalUsing.cs
+++ FFmpegArgs.Cores/GlobalUsing.cs
@@ -16,3 +16,6 @@ global using System.Collections.Generic;
 global using System.Linq;
 global using System.Drawing;
 global using System.ComponentModel;
+#if NET5_0_OR_GREATER
+global using System.Diagnostics.CodeAnalysis;
+#endif
