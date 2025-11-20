7c85050335e9f1e82560cb9d05b7c0253588e968
tqk2811 <tqk2811@gmail.com>
2025-11-21 01:55:45 +0700

    add BaseMuxer

---
+++ FFmpegArgs.Outputs.Muxers/BaseMuxer.cs
@@ -0,0 +1,15 @@
+﻿namespace FFmpegArgs.Inputs.Demuxers
+{
+    public abstract class BaseMuxer : BaseOption, IMux
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="format"></param>
+        /// <param name="baseInput"></param>
+        protected BaseMuxer(MuxingFileFormat format, BaseOutput baseInput) : base(baseInput)
+        {
+            this.Format(format);
+        }
+    }
+}
---
+++ FFmpegArgs.Outputs.Muxers/GlobalUsing.cs
@@ -0,0 +1,13 @@
+﻿global using System;
+global using System.IO;
+global using System.Linq;
+global using System.Collections.Generic;
+
+global using FFmpegArgs.Cores.Attributes;
+global using FFmpegArgs.Cores.Utils;
+global using FFmpegArgs.Cores;
+global using FFmpegArgs.Cores.Interfaces;
+global using FFmpegArgs.Cores.Enums;
+global using FFmpegArgs.Cores.Maps;
+global using FFmpegArgs.Cores.Outputs;
+global using FFmpegArgs.Cores.Streams;
