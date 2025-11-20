a646256c5bb8f4460910070e4782dc05bdf833c6
tqk2811 <tqk2811@gmail.com>
2025-11-20 23:59:46 +0700

    add new project FFmpegArgs.Inputs.Demuxers, FFmpegArgs.Outputs.Muxers

---
+++ FFmpegArgs.Inputs.Demuxers/Nuget.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module ..\BuildSingle.psm1
+$dirInfo= New-Object -Typename System.IO.DirectoryInfo -ArgumentList ([System.IO.Directory]::GetCurrentDirectory())
+$projectName= $dirInfo.Name;
+
+NugetBuildSingle $projectName
