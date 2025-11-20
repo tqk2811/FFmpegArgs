ef2f5bae57c2f5690ae96202a90fc2f8dc6464dd
tqk2811 <tqk2811@gmail.com>
2025-11-21 01:31:38 +0700

    init FFmpegArgs.Inputs.Demuxers

---
+++ FFmpegArgs.Inputs.Demuxers/BaseDemuxer.cs
@@ -0,0 +1,20 @@
+﻿using FFmpegArgs.Cores;
+using FFmpegArgs.Cores.Enums;
+using FFmpegArgs.Cores.Inputs;
+using FFmpegArgs.Cores.Interfaces;
+
+namespace FFmpegArgs.Inputs.Demuxers
+{
+    public abstract class BaseDemuxer : BaseOption, IDemux
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="format"></param>
+        /// <param name="baseInput"></param>
+        protected BaseDemuxer(DemuxingFileFormat format, BaseInput baseInput) : base(baseInput)
+        {
+            this.Format(format);
+        }
+    }
+}
---
+++ FFmpegArgs.Inputs.Demuxers/GlobalUsing.cs
@@ -0,0 +1,15 @@
+﻿global using System;
+global using System.IO;
+global using System.Linq;
+global using System.Collections;
+global using System.Collections.Generic;
+global using System.Drawing;
+
+global using FFmpegArgs.Cores.Streams;
+global using FFmpegArgs.Cores;
+global using FFmpegArgs.Cores.Enums;
+global using FFmpegArgs.Cores.Exceptions;
+global using FFmpegArgs.Cores.Inputs;
+global using FFmpegArgs.Cores.Filters;
+global using FFmpegArgs.Cores.Attributes;
+global using FFmpegArgs.Cores.Interfaces;
