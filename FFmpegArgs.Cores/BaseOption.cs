9ac4c915d87f9a8d9c4fb4f39dc9f578adfbb153
tqk2811 <tqk2811@gmail.com>
2025-11-21 01:18:56 +0700

    BaseOptionExtension add SetOption for Size

--- FFmpegArgs.Cores/BaseOption.cs
+++ FFmpegArgs.Cores/BaseOption.cs
@@ -62,19 +62,49 @@
         /// <exception cref="ArgumentNullException"></exception>
         public static T SetOption<T>(
             this T baseOption,
-            string key, 
+            string key,
             string val,
             bool throwIfDuplicate = BaseOption.DEFAULT_ThrowIfDuplicate
-            ) 
+            )
             where T : BaseOption
         {
-            if (string.IsNullOrEmpty(key)) 
+            if (string.IsNullOrEmpty(key))
                 throw new ArgumentNullException(nameof(key));
-            if (string.IsNullOrEmpty(val)) 
+            if (string.IsNullOrEmpty(val))
                 throw new ArgumentNullException(nameof(val));
             if (throwIfDuplicate && baseOption._options.ContainsKey(key))
                 throw new InvalidOperationException($"Option '{key}' already exists with value {baseOption._options[key]}");
             baseOption._options[key] = val;
+            return baseOption;
+        }
+        /// <summary>
+        /// Adds or updates an option with a size value in the specified option set using the given key.
+        /// </summary>
+        /// <remarks>The size value is stored as a string in the format "{Width}x{Height}". This method
+        /// enables fluent configuration of option sets by returning the updated instance.</remarks>
+        /// <typeparam name="T">The type of option set to update. Must inherit from <see cref="BaseOption"/>.</typeparam>
+        /// <param name="baseOption">The option set to which the size option will be added or updated.</param>
+        /// <param name="key">The key that identifies the option. Cannot be null or empty.</param>
+        /// <param name="size">The <see cref="Size"/> value to associate with the specified key.</param>
+        /// <param name="throwIfDuplicate">If <see langword="true"/>, throws an exception if the key already exists; otherwise, overwrites the existing
+        /// value. The default is <see langword="true"/>.</param>
+        /// <returns>The updated option set with the specified size option added or updated.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="key"/> is null or empty.</exception>
+        /// <exception cref="InvalidOperationException">Thrown if <paramref name="throwIfDuplicate"/> is <see langword="true"/> and the specified key already exists
+        /// in the option set.</exception>
+        public static T SetOption<T>(
+            this T baseOption,
+            string key,
+            Size size,
+            bool throwIfDuplicate = BaseOption.DEFAULT_ThrowIfDuplicate
+            )
+            where T : BaseOption
+        {
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentNullException(nameof(key));
+            if (throwIfDuplicate && baseOption._options.ContainsKey(key))
+                throw new InvalidOperationException($"Option '{key}' already exists with value {baseOption._options[key]}");
+            baseOption._options[key] = $"{size.Width}x{size.Height}";
             return baseOption;
         }
         /// <summary>
@@ -89,15 +119,15 @@
         /// <exception cref="ArgumentNullException"></exception>
         public static T SetOption<T>(
             this T baseOption,
-            string key, 
+            string key,
             object val,
             bool throwIfDuplicate = BaseOption.DEFAULT_ThrowIfDuplicate
-            ) 
+            )
             where T : BaseOption
         {
-            if (string.IsNullOrEmpty(key)) 
+            if (string.IsNullOrEmpty(key))
                 throw new ArgumentNullException(nameof(key));
-            if (val is null) 
+            if (val is null)
                 throw new ArgumentNullException(nameof(val));
             return baseOption.SetOption(key, val.ToString(), throwIfDuplicate);
         }
@@ -115,18 +145,18 @@
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="InvalidRangeException"></exception>
         public static T SetOptionRange<T>(
-            this T baseOption, 
-            string key, 
-            int val, 
-            int min, 
-            int max, 
+            this T baseOption,
+            string key,
+            int val,
+            int min,
+            int max,
             bool throwIfDuplicate = BaseOption.DEFAULT_ThrowIfDuplicate
-            ) 
+            )
             where T : BaseOption
         {
-            if (string.IsNullOrEmpty(key)) 
+            if (string.IsNullOrEmpty(key))
                 throw new ArgumentNullException(nameof(key));
-            if (val < min || val > max) 
+            if (val < min || val > max)
                 throw new InvalidRangeException($"Range Required: {min} <= {key} <= {max}");
             return baseOption.SetOption(key, val.ToString(), throwIfDuplicate);
         }
@@ -144,18 +174,18 @@
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="InvalidRangeException"></exception>
         public static T SetOptionRange<T>(
-            this T baseOption, 
-            string key, 
-            float val, 
-            float min, 
-            float max, 
+            this T baseOption,
+            string key,
+            float val,
+            float min,
+            float max,
             bool throwIfDuplicate = BaseOption.DEFAULT_ThrowIfDuplicate
-            ) 
+            )
             where T : BaseOption
         {
-            if (string.IsNullOrEmpty(key)) 
+            if (string.IsNullOrEmpty(key))
                 throw new ArgumentNullException(nameof(key));
-            if (float.IsNaN(val) || val < min || val > max) 
+            if (float.IsNaN(val) || val < min || val > max)
                 throw new InvalidRangeException($"Range Required: {min} <= {key} <= {max}");
             return baseOption.SetOption(key, val.ToString(), throwIfDuplicate);
         }
@@ -173,18 +203,18 @@
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="InvalidRangeException"></exception>
         public static T SetOptionRange<T>(
-            this T baseOption, 
-            string key, 
-            double val, 
-            double min, 
-            double max, 
+            this T baseOption,
+            string key,
+            double val,
+            double min,
+            double max,
             bool throwIfDuplicate = BaseOption.DEFAULT_ThrowIfDuplicate
-            ) 
+            )
             where T : BaseOption
         {
-            if (string.IsNullOrEmpty(key)) 
+            if (string.IsNullOrEmpty(key))
                 throw new ArgumentNullException(nameof(key));
-            if (double.IsNaN(val) || val < min || val > max) 
+            if (double.IsNaN(val) || val < min || val > max)
                 throw new InvalidRangeException($"Range Required: {min} <= {key} <= {max}");
             return baseOption.SetOption(key, val.ToString(), throwIfDuplicate);
         }
@@ -202,18 +232,18 @@
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="InvalidRangeException"></exception>
         public static T SetOptionRange<T>(
-            this T baseOption, 
-            string key, 
-            long val, 
-            long min, 
-            long max, 
+            this T baseOption,
+            string key,
+            long val,
+            long min,
+            long max,
             bool throwIfDuplicate = BaseOption.DEFAULT_ThrowIfDuplicate
-            ) 
+            )
             where T : BaseOption
         {
-            if (string.IsNullOrEmpty(key)) 
+            if (string.IsNullOrEmpty(key))
                 throw new ArgumentNullException(nameof(key));
-            if (val < min || val > max) 
+            if (val < min || val > max)
                 throw new InvalidRangeException($"Range Required: {min} <= {key} <= {max}");
             return baseOption.SetOption(key, val.ToString(), throwIfDuplicate);
         }
@@ -231,18 +261,18 @@
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="InvalidRangeException"></exception>
         public static T SetOptionRange<T>(
-            this T baseOption, 
-            string key, 
-            decimal val, 
-            decimal min, 
-            decimal max, 
+            this T baseOption,
+            string key,
+            decimal val,
+            decimal min,
+            decimal max,
             bool throwIfDuplicate = BaseOption.DEFAULT_ThrowIfDuplicate
-            ) 
+            )
             where T : BaseOption
         {
-            if (string.IsNullOrEmpty(key)) 
+            if (string.IsNullOrEmpty(key))
                 throw new ArgumentNullException(nameof(key));
-            if (val < min || val > max) 
+            if (val < min || val > max)
                 throw new InvalidRangeException($"Range Required: {min} <= {key} <= {max}");
             return baseOption.SetOption(key, val.ToString(), throwIfDuplicate);
         }
@@ -263,15 +293,15 @@
             this T baseOption,
             string key,
             TimeSpan val,
-            TimeSpan min, 
+            TimeSpan min,
             TimeSpan max,
             bool throwIfDuplicate = BaseOption.DEFAULT_ThrowIfDuplicate
-            ) 
+            )
             where T : BaseOption
         {
-            if (string.IsNullOrEmpty(key)) 
+            if (string.IsNullOrEmpty(key))
                 throw new ArgumentNullException(nameof(key));
-            if (val < min || val > max) 
+            if (val < min || val > max)
                 throw new InvalidRangeException($"Range Required: {min:hh\\:mm\\:ss\\.fff} <= {key:hh\\:mm\\:ss\\.fff} <= {max:hh\\:mm\\:ss\\.fff}");
             return baseOption.SetOption(key, val.ToString(), throwIfDuplicate);
         }
